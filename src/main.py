09a2cbe1ec9681aba3f8926b69213ef403584444
Zigleur <eden.douru@hotmail.com>
2024-05-07 16:43:36 +0200

    base working

--- src/main.py
+++ src/main.py
@@ -2,12 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.header("Test Header")
-
-st.title("Test Title")
+st.title(":factory: DataSet Factory")
 
 def uploadJsonFile():
-    uploadedFile = st.file_uploader("Upload a dataset file", type=['json', 'jsonl'], accept_multiple_files=False, key=None, help="Upload datasets in json / jsonl format")
+    uploadedFile = st.file_uploader("Upload a dataset file", type=['json', 'jsonl'], accept_multiple_files=False, help="Upload datasets in json / jsonl format")
     if st.button("Submit"):
         if uploadedFile is not None:
             Mainwindow(uploadedFile)
@@ -17,6 +15,9 @@ def uploadJsonFile():
 
 def Mainwindow(uploadedFile):
     with st.container(border=True):
-        st.dataframe(uploadedFile, use_container_width=True)
+        st.header(uploadedFile.name)
+        df = pd.read_json(uploadedFile, lines=True)
+        st.dataframe(df, use_container_width=True)
+
 
 uploadJsonFile()
