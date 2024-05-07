b6228ea24ca9dab5200b83fcee9fa35090ae1c44
Zigleur <eden.douru@hotmail.com>
2024-05-08 00:39:56 +0200

    + config.toml & layout enhancement

--- src/main.py
+++ src/main.py
@@ -4,33 +4,31 @@ import numpy as np
 
 st.set_page_config(page_title="DataSet Factory", page_icon=":factory:", layout="wide", menu_items={"Get Help": 'https://github.com/Shrayzz/py_Jeu-de-Donnees/blob/main/README.md', "About": 'https://github.com/Shrayzz/py_Jeu-de-Donnees'})
 
-st.title(":factory: DataSet Factory")
+st.title(":factory: DataSet Factory", anchor=False)
 
-col1, col2 = st.columns(2)
+topCol1, topCol2 = st.columns([0.75,0.25])
+bottomCol1, bottomCol2, bottomCol3 = st.columns([0.3,0.4,0.3])
 
 def uploadJsonFile():
-    with st.container(border=True):
-        uploadedFile = st.file_uploader("Upload a dataset file", type=['json', 'jsonl'], accept_multiple_files=False, help="Upload datasets in json / jsonl format",)
-    if st.button("Submit"):
-        if uploadedFile is not None:
-            Mainwindow(uploadedFile)
-        else:
-            st.toast(":warning: Please upload a file")
+    with bottomCol2:
+        uploadedFile = st.file_uploader("Load a dataset file", type=['json', 'jsonl'], accept_multiple_files=False, help="Upload datasets in json / jsonl format",)
+        if st.button(":outbox_tray: Load dataset"):
+            if uploadedFile is not None:
+                Mainwindow(uploadedFile)
+            else:
+                st.toast(":warning: You must choose a file to upload !")
 
 def Mainwindow(uploadedFile):
-    with col1:
+    with topCol1:
         st.header(uploadedFile.name)
-        df = pd.read_json(uploadedFile, lines=True)
-        st.dataframe(df, use_container_width=True)
-    with col2:
-        st.header("Actions")
-
-    
-
-
-    # with st.container(border=True):
-    #     st.header(uploadedFile.name)
-    #     df = pd.read_json(uploadedFile, lines=True)
-    #     st.dataframe(df, use_container_width=True)
+        df = pd.read_json(uploadedFile) # some JSON needs the args lines=True and some not, depends on the JSON file format
+        st.dataframe(df, use_container_width=True, height=495)
+    with topCol2:
+        st.header("DataSet Informations")
+        with st.container(border=True):
+            st.write("Rows: ", df.shape[0])
+            st.write("Columns: ", df.shape[1])
+            st.write("Columns Names: ", df.columns.tolist())
+            st.write("Columns Unique Values: ", df.nunique())
 
 uploadJsonFile()
