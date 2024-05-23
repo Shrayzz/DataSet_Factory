5fb00549161f0810db824a4a9ef1fc1c342acd00
HubertTom <113830307+Pomolokoki@users.noreply.github.com>
2024-05-23 11:46:38 +0200

    Update db.py (affichage db + validation

--- src/db.py
+++ src/db.py
@@ -5,16 +5,28 @@ import sqlite3
 
 con = sqlite3.connect("dataset.db", check_same_thread=False)
 
-def createDB(df):
+def GetDfFromDb(sql, df):
+    if sql == "":
+        sqlcol = " "
+        for col in df.columns:
+            sqlcol += col
+            if col != df.columns[-1]:
+                sqlcol+= ", "
+
+        sql = '''SELECT''' + sqlcol + " FROM dataset"
+    con.commit()
+    df = pd.read_sql_query(sql, con)
+    return df
+
+def CreateDB(df):
 
     col = len(df.columns)
     
     sqlcreate = '''CREATE TABLE dataset ( id INTEGER PRIMARY KEY AUTOINCREMENT'''
-    
-    print(df.columns)
+
     for col in df.columns:
         sqlcreate += ", " + col + " VARCHAR(500)" 
-    sqlcreate+=" ) "
+    sqlcreate+=", isValidate BOOLEAN ) "
 
     cursor = con.cursor()
     
@@ -28,12 +40,20 @@ def createDB(df):
             sqlinsert += ((df.iloc[row,col]).replace('"', '""')).replace("'", "''")
             if col != len(df.columns)-1:
                 sqlinsert += '", "'
-        sqlinsert += '")'
+        sqlinsert += '", FALSE)'
         cursor.execute(sqlinsert)
 
-    con.commit()
     
+    sqlcol = " "
+    for col in df.columns:
+        sqlcol += col
+        if col != df.columns[-1]:
+            sqlcol+= ", "
 
+    sql = '''SELECT''' + sqlcol + ", isValidate FROM dataset"
+    return GetDfFromDb(sql, df)
+
+# searching : liste d'expression / colu : liste de colonnes
 def SearchInDB(searching, colu, df):
     sqlin = ''' LIKE ("'''
     for ex in searching:
@@ -47,7 +67,7 @@ def SearchInDB(searching, colu, df):
         if col != df.columns[-1]:
             sqlcol+= ", "
 
-    sql = '''SELECT''' + sqlcol + ", isValidate FROM dataset"
+    sql = '''SELECT''' + sqlcol + " FROM dataset"
     
     sql+= " WHERE "
 
@@ -66,7 +86,18 @@ def SearchInDB(searching, colu, df):
         if (col != colu[-1]):
             sql+=') OR ('
     sql+=')'
-    print(sql)
-    df = pd.read_sql_query(sql, con)
-    st.write(sql)
-    return df
+    return GetDfFromDb(sql, df)
+
+def Validate(row):
+    cursor = con.cursor()
+    con.execute("SELECT isValidate FROM dataset WHERE id = " + str(row))
+    result = cursor.fetchall()
+    
+    st.write("SELECT isValidate FROM dataset WHERE id = " + str(row))
+    st.write(result)
+    if result == True:
+        sql = '''UPDATE dataset SET isValidate = FALSE WHERE id = ''' + str(row)
+    sql = '''UPDATE dataset SET isValidate = TRUE WHERE id = ''' + str(row)
+    result = con.execute(sql)
+    con.commit()
+
