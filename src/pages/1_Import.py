026a0daa8ab3d0d08558d7810e4fe8c21183dbbd
Axel <103132558+Shrayzz@users.noreply.github.com>
2024-06-10 11:33:13 +0200

    🛠 Not important fixes

--- src/pages/1_Import.py
+++ src/pages/1_Import.py
@@ -7,7 +7,7 @@ import tempfile
 
 st.set_page_config(page_title="DataSet Factory - Upload a file", page_icon=":factory:", layout="wide", menu_items={"Get Help": 'https://github.com/Shrayzz/py_Jeu-de-Donnees/blob/main/README.md', "About": 'https://github.com/Shrayzz/py_Jeu-de-Donnees'})
 
-st.title(":outbox_tray: Import a file", anchor=False)
+st.title(":inbox_tray: Import a file", anchor=False)
 
 topCol0, topCol1 = st.columns([0.75,0.25])
 
@@ -15,8 +15,8 @@ bottomCol0, bottomCol1, bottomCol2 = st.columns([0.3,0.4,0.3])
 
 def uploadJsonFile(): # upload a file to display it in a dataset
     with bottomCol1:
-        uploadedFile = st.file_uploader("Load a dataset file", type=['json', 'jsonl'], accept_multiple_files=False, help="Upload datasets in json / jsonl format",)
-        if st.button(":outbox_tray: Load a dataset"):
+        uploadedFile = st.file_uploader("Load a dataset file", type=['json', 'jsonl'], accept_multiple_files=False, help="Upload datasets in JSON / JSONL format only",)
+        if st.button(":inbox_tray: Load a dataset"):
             if uploadedFile is not None:
                 # Save the uploaded file temporarily
                 temp_file_path = os.path.join(tempfile.gettempdir(), "temp_uploaded_file.json")
@@ -40,8 +40,8 @@ def uploadJsonFile(): # upload a file to display it in a dataset
                 st.session_state['dataframe'] = df
                 
                 # Redirect to Edit page
-                st.success("File uploaded successfully, look on Edit tab !")
+                st.success(":heavy_check_mark: File uploaded successfully, look on Edit tab !")
             else:
-                st.warning("You must choose a file to upload !")
+                st.warning(":warning: You must choose a file to upload !")
 
 uploadJsonFile()
